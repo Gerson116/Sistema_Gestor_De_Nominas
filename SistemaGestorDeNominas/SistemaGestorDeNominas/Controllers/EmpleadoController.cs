7b212d005743b6a671140b80da6f156940bf47c2
gersondevx <gerson.santosm3@gmail.com>
2020-07-20 12:29:28 -0400

    Mejoras en las vistas.

--- SistemaGestorDeNominas/SistemaGestorDeNominas/Controllers/EmpleadoController.cs
+++ SistemaGestorDeNominas/SistemaGestorDeNominas/Controllers/EmpleadoController.cs
@@ -9,27 +9,50 @@ namespace SistemaGestorDeNominas.Controllers
     [Route("[controller]/[action]")]
     public class EmpleadoController : Controller
     {
+        [HttpGet]
         public ActionResult AgregarEmpleado() 
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult AgregarEmpleado(string nombre, string apellido, string sexo, double sueldo, string fecha_de_entrada)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditarEmpleado(int id_empleado)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditarEmpleado(string nombre, string apellido, string sexo, double sueldo, DateTime fecha_de_entrada)
+        {
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult ListadoEmpleado()
         {
+            // el listado siempre llamara los empleados que estan activos.
             return View();
         }
 
-        public ActionResult EditarEmpleado()
+        public ActionResult ListadoEmpleado(int estado_A_Filtrar)
         {
+            // 1 para activo y 0 para inactivo
             return View();
         }
 
-        public ActionResult CambiarEstado_A_Activo()
+        public ActionResult CambiarEstado_A_Activo(int id_empleado)
         {
             // Al eliminar los datos esta acción redireccionara a la accion lista.
             return RedirectToAction(nameof(ListadoEmpleado));
         }
-        public ActionResult CambiarEstado_A_Inactivo()
+
+        public ActionResult CambiarEstado_A_Inactivo(int id_empleado)
         {
             // Al eliminar los datos esta acción redireccionara a la accion lista.
             return RedirectToAction(nameof(ListadoEmpleado));
