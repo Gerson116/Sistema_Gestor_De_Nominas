d6f6baab6b8c2a9c909653de9a07b86ce0d012d6
gersondevx <gerson.santosm3@gmail.com>
2020-07-21 11:57:08 -0400

    Retenciones calculadas.

--- SistemaGestorDeNominas/SistemaGestorDeNominas/Models/ContribucionEmpleado.cs
+++ SistemaGestorDeNominas/SistemaGestorDeNominas/Models/ContribucionEmpleado.cs
@@ -9,11 +9,8 @@ namespace SistemaGestorDeNominas.Models
     public class ContribucionEmpleado
     {
         public int ID { get; set; }
-        [Required]
         public double ARS { get; set; }
-        [Required]
         public double AFP { get; set; }
-        [Required]
         public double IRS { get; set; }
     }
 }
---
+++ SistemaGestorDeNominas/SistemaGestorDeNominas/Services/Contribucion_Y_Otros_Impuestos/ValidarLasContribuciones.cs
@@ -0,0 +1,48 @@
+﻿using SistemaGestorDeNominas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaGestorDeNominas.Services.Contribucion_Y_Otros_Impuestos
+{
+    public class ValidarLasContribuciones
+    {
+        private double _arsEmpleador;
+        private double _afpEmpleador;
+        private double _riesgoLaboral;
+
+        private double _arsEmpleado;
+        private double _afpEmpleado;
+
+        private CalcularIRS _objCalcularIRS;
+        private ReglaDeTresParaCalcularPorcientos _objCalcularPorcientos;
+
+        public ValidarLasContribuciones()
+        {
+            _arsEmpleador = 7.09;
+            _afpEmpleador = 7.10;
+            _riesgoLaboral = 1.03;
+
+            _arsEmpleado = 3.04;
+            _afpEmpleado = 2.87;
+
+            _objCalcularIRS = new CalcularIRS();
+            _objCalcularPorcientos = new ReglaDeTresParaCalcularPorcientos();
+        }
+        public ContribucionEmpleado ValidarContribucionDelEmpleado(double sueldo)
+        {
+            var objContribucionEmpleado = new ContribucionEmpleado();
+            objContribucionEmpleado.ARS = _objCalcularPorcientos.ARS(sueldo, _arsEmpleado);
+            objContribucionEmpleado.AFP = _objCalcularPorcientos.AFP(sueldo, _afpEmpleado);
+            objContribucionEmpleado.IRS = _objCalcularIRS.ResultadoIRSEmpleado(sueldo);
+            return objContribucionEmpleado;
+        }
+
+        public ContribucionEmpleador ValidarContribucionDelEmpleador()
+        {
+            var objContribucionEmpleador = new ContribucionEmpleador();
+            return null;
+        }
+    }
+}
